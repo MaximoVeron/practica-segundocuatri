b712b2dc4d789bf942561a73537300479f3c3d4c
Maximo Veron <maximoveron49@gmail.com>
2025-08-23 23:29:45 -0300

    parte usuarios completa

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -23,3 +23,41 @@ export const getUserById = async (req, res) => {
         res.status(500).json({ error: "Error del servidor" });
     }
 };
+
+export const createUser = async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error("error al crear el usuario", error);
+        res.status(500).json({ error: "Error del servidor" });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const usuario = await User.findByPk(req.params.id);
+        if (usuario){
+            await usuario.destroy();
+            res.json({ message: "Usuario eliminado" });
+        } else {
+            res.status(404).json({ error: "Usuario no encontrado" });
+        }
+    } catch (error) {
+        console.error("error al eliminar el usuario", error);
+        res.status(500).json({ error: "Error del servidor" });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const usuario = await User.findByPk(req.params.id);
+        if (usuario){
+            await usuario.update(req.body);
+            res.json(usuario);
+        }
+    } catch (error){
+        console.error("error al actualizar el usuario", error);
+        res.status(500).json({ error: "Error del servidor" });
+    }
+};
